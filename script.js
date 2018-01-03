6f725a44663e132c504a58d07d570aad93540cd1
Rudy Perez <rudy90perez@gmail.com>
2018-01-02 18:27:03 -0600

    added more css stylings, took rows off and only added divs. Added erase feature

--- script.js
+++ script.js
@@ -19,4 +19,29 @@ function createDivs(){
 	};
 };
 
-window.onload = createDivs;
+function createCols(){
+	for(var i = 0; i < 256; i++){
+		const container = document.querySelector('.container');
+		const div = document.createElement('div');
+		div.classList.add('col-1');
+		container.appendChild(div);
+					const divs = document.querySelectorAll('.col-1');
+					divs.forEach((div) => {
+						div.addEventListener('mouseenter', (e) => {
+							e.target.style.background = '#E9D460';
+						});
+					});
+
+	};
+};
+
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+	//const userNumber = prompt('Enter a number.');
+		const divs = document.querySelectorAll('.col-1');
+		divs.forEach((div) => {
+			div.style.background = '#BE90D4';
+		})
+});
+
+window.onload = createCols;
